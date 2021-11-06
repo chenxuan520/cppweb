c886a3b8129ec4154c1d67adefb201380cb2f40b
chaiquan <1143663140@qq.com>
2021-11-07 00:07:50 +0800

    addEmail

--- Server.cpp
+++ Server.cpp
@@ -963,7 +963,7 @@ public:
 	}
 	const char* decryptToken(const char* key,const char* token,char* buffer,unsigned int bufferLen)
 	{
-		char* temp=strchr((char*)token,'&');
+		char* temp=strrchr((char*)token,'&');
 		if(temp==NULL||key==NULL||token==NULL||buffer==NULL||bufferLen<strlen(token))
 		{
 			sprintf(err,"input wrong");
@@ -1007,6 +1007,244 @@ public:
 	inline const char* LastError()
 	{
 		return err;
+	}
+};
+/********************************
+	author:chenxuan
+	date:2021/11/7
+	funtion:create a email send class
+*********************************/
+class Email{
+private:
+	sockaddr_in their_addr;
+	bool isDebug;
+	char error[30];
+    struct Base64Date6
+    {
+        unsigned int d4 : 6;
+        unsigned int d3 : 6;
+        unsigned int d2 : 6;
+        unsigned int d1 : 6;
+    };
+public:
+	Email(const char* domain,bool debug=false)
+	{
+		isDebug=debug;
+		memset(error,0,sizeof(char)*30);
+        memset(&their_addr, 0, sizeof(their_addr));
+        their_addr.sin_family = AF_INET;
+        their_addr.sin_port = htons(25);    
+        hostent* hptr = gethostbyname(domain);          
+        memcpy(&their_addr.sin_addr.s_addr, hptr->h_addr_list[0], hptr->h_length);
+	}
+	bool emailSend(const char* sendEmail,const char* passwd,const char* recEmail,const char* body)
+	{
+      	int sockfd = 0;
+      	char recBuffer[1000]={0},senBuffer[1000]={0},login[128],pass[128];
+        sockfd = socket(PF_INET, SOCK_STREAM, 0);
+        if (sockfd < 0)
+        {
+            sprintf(error,"open socket wrong");
+            return false;
+        }
+        if (connect(sockfd,(struct sockaddr *)&their_addr, sizeof(struct sockaddr)) < 0)
+        {
+            sprintf(error,"connect wrong");
+		    return false;
+        }
+        memset(recBuffer,0,sizeof(char)*1000);
+        while (recv(sockfd, recBuffer, 1000, 0) <= 0)
+        {
+            if(isDebug)
+            	printf("reconnecting\n");
+            sleep(2);
+            sockfd = socket(PF_INET, SOCK_STREAM, 0);
+			if (sockfd < 0)
+	        {
+	            sprintf(error,"open socket wrong");
+	            return false;
+	        }
+	        if (connect(sockfd,(struct sockaddr *)&their_addr, sizeof(struct sockaddr)) < 0)
+	        {
+	            sprintf(error,"connect wrong");
+			    return false;
+	        }
+            memset(recBuffer, 0, 1000);
+        }
+        if(isDebug)
+        	printf("get:%s",recBuffer);
+        
+        memset(senBuffer, 0, 1000);
+        sprintf(senBuffer, "EHLO HYL-PC\r\n");
+        send(sockfd, senBuffer, strlen(senBuffer), 0);
+        memset(recBuffer, 0, 1000);
+        recv(sockfd, recBuffer, 1000, 0);
+        if(isDebug)
+        	printf("EHLO REceive:%s\n",recBuffer);
+        
+        memset(senBuffer, 0, 1000);
+        sprintf(senBuffer, "AUTH LOGIN\r\n");
+        send(sockfd, senBuffer, strlen(senBuffer), 0);
+        memset(recBuffer, 0, 1000);
+        recv(sockfd, recBuffer, 1000, 0);
+        if(isDebug)
+        	printf("Auth Login Receive:%s\n",recBuffer);
+        
+		memset(senBuffer, 0, 1000);
+        sprintf(senBuffer, "%s",sendEmail);
+        memset(login, 0, 128);
+        EncodeBase64(login, senBuffer, strlen(senBuffer));
+        sprintf(senBuffer, "%s\r\n", login);
+        send(sockfd, senBuffer, strlen(senBuffer), 0);
+    	if(isDebug)
+    		printf("Base64 UserName:%s\n",senBuffer);
+        memset(recBuffer, 0, 1000);
+        recv(sockfd, recBuffer, 1000, 0);
+        if(isDebug)
+        	printf("User Login Receive:%s\n",recBuffer);
+
+		sprintf(senBuffer, "%s",passwd);//password
+        memset(pass, 0, 128);
+        EncodeBase64(pass, senBuffer, strlen(senBuffer));
+        sprintf(senBuffer, "%s\r\n", pass);
+        send(sockfd, senBuffer, strlen(senBuffer), 0);
+        if(isDebug)
+        	printf("Base64 Password:%s\n",senBuffer);
+
+        memset(recBuffer, 0, 1000);
+        recv(sockfd, recBuffer, 1000, 0);
+        if(isDebug)
+        	printf("Send Password Receive:%s\n",recBuffer);
+
+        // self email
+        memset(recBuffer, 0, 1000);
+        sprintf(senBuffer, "MAIL FROM: <%s>\r\n",sendEmail);  
+        send(sockfd, senBuffer, strlen(senBuffer), 0);
+        memset(recBuffer, 0, 1000);
+        recv(sockfd, recBuffer, 1000, 0);
+        if(isDebug)
+        	printf("set Mail From Receive:%s\n",recBuffer);
+
+        // recv email
+        sprintf(recBuffer, "RCPT TO:<%s>\r\n", recEmail);
+        send(sockfd, recBuffer, strlen(recBuffer), 0);
+        memset(recBuffer, 0, 1000);
+        recv(sockfd, recBuffer, 1000, 0);
+        if(isDebug)
+        	printf("Tell Sendto Receive:%s\n",recBuffer);
+        int bug=0;
+        sscanf(recBuffer,"%d",&bug);
+        if(bug==550)
+        {
+			sprintf(error,"recemail wrong");
+            return false;
+		}
+        // send body
+        sprintf(senBuffer, "DATA\r\n");
+        send(sockfd, senBuffer, strlen(senBuffer), 0);
+        memset(recBuffer, 0, 1000);
+        recv(sockfd, recBuffer, 1000, 0);
+        if(isDebug)
+        	printf("Send Mail Prepare Receive:%s\n",recBuffer);
+
+        // send data
+        sprintf(senBuffer, "%s\r\n.\r\n", body);
+        send(sockfd, senBuffer, strlen(senBuffer), 0);
+        memset(recBuffer, 0, 1000);
+        recv(sockfd, recBuffer, 1000, 0);
+        if(isDebug)
+        	printf("Send Mail Receive:%s\n",recBuffer);
+
+        // QUIT
+        sprintf(senBuffer,"QUIT\r\n");
+        send(sockfd, senBuffer, strlen(senBuffer), 0);
+        memset(recBuffer, 0, 1000);
+        recv(sockfd, recBuffer, 1000, 0);
+        if(isDebug)
+        	printf("Quit Receive:%s\n",recBuffer);
+        return true;
+	}
+	char ConvertToBase64(char uc)
+    {
+        if (uc < 26)
+            return 'A' + uc;
+        if (uc < 52)
+            return 'a' + (uc - 26);
+        if (uc < 62)
+            return '0' + (uc - 52);
+        if (uc == 62)
+            return '+';
+        return '/';
+    }
+    void EncodeBase64(char *dbuf, char *buf128, int len)
+    {
+        struct Base64Date6 *ddd = NULL;
+        int i = 0;
+        char buf[256] = { 0 };
+        char* tmp = NULL;
+        char cc = '\0';
+        memset(buf, 0, 256);
+        strcpy(buf, buf128);
+        for (i = 1; i <= len / 3; i++)
+        {
+            tmp = buf + (i - 1) * 3;
+            cc = tmp[2];
+            tmp[2] = tmp[0];
+            tmp[0] = cc;
+            ddd = (struct Base64Date6 *)tmp;
+            dbuf[(i - 1) * 4 + 0] = ConvertToBase64((unsigned int)ddd->d1);
+            dbuf[(i - 1) * 4 + 1] = ConvertToBase64((unsigned int)ddd->d2);
+            dbuf[(i - 1) * 4 + 2] = ConvertToBase64((unsigned int)ddd->d3);
+            dbuf[(i - 1) * 4 + 3] = ConvertToBase64((unsigned int)ddd->d4);
+        }
+        if (len % 3 == 1)
+        {
+            tmp = buf + (i - 1) * 3;
+            cc = tmp[2];
+            tmp[2] = tmp[0];
+            tmp[0] = cc;
+            ddd = (struct Base64Date6 *)tmp;
+            dbuf[(i - 1) * 4 + 0] = ConvertToBase64((unsigned int)ddd->d1);
+            dbuf[(i - 1) * 4 + 1] = ConvertToBase64((unsigned int)ddd->d2);
+            dbuf[(i - 1) * 4 + 2] = '=';
+            dbuf[(i - 1) * 4 + 3] = '=';
+        }
+        if (len % 3 == 2)
+        {
+            tmp = buf + (i - 1) * 3;
+            cc = tmp[2];
+            tmp[2] = tmp[0];
+            tmp[0] = cc;
+            ddd = (struct Base64Date6 *)tmp;
+            dbuf[(i - 1) * 4 + 0] = ConvertToBase64((unsigned int)ddd->d1);
+            dbuf[(i - 1) * 4 + 1] = ConvertToBase64((unsigned int)ddd->d2);
+            dbuf[(i - 1) * 4 + 2] = ConvertToBase64((unsigned int)ddd->d3);
+            dbuf[(i - 1) * 4 + 3] = '=';
+        }
+        return;
+    }
+    void CreateSend(const char* youName,const char* toName,const char* from,const char* to,const char* subject,const char* body,char* buf)
+    {
+        sprintf(buf,"From: \"%s\"<%s>\r\n"
+                "To: \"%s\"<%s>\r\n"
+                "Subject:%s\r\n\r\n"
+                "%s\n",youName,from,toName,to,subject,body);
+    }
+    inline const char* LastError()
+    {
+		return error;
+	}
+	static const char* getDomainBySelfEmail(const char* email,char* buffer,int bufferLen)
+	{
+		char* temp=strrchr((char*)email,'@');
+		if(temp==NULL)
+			return NULL;
+		if(bufferLen<=20)
+			return NULL;
+		if(strlen(temp)>15)
+			return NULL;
+		sprintf(buffer,"smtp.%s",temp+1);
+		return buffer;
 	}
 };
 /********************************
