8eae06f42f285fa4dd20ed700f42b5031fee68a0
chenxuan <1143663140@qq.com>
2022-01-25 18:50:12 +0800

    deletewebtoken

--- hpp/cppweb.h
+++ hpp/cppweb.h
@@ -1859,104 +1859,6 @@ public:
 		return srcString;
 	}
 };
-class WebToken{
-private:
-	char* backString;
-	char err[30];
-public:
-	WebToken()
-	{
-		backString=NULL;
-		memset(err,0,sizeof(char)*30);
-		sprintf(err,"no error");
-	}
-	const char* createToken(const char* key,const char* encryption,char* getString,unsigned int stringLen,unsigned int liveSecond)
-	{
-		int temp=0;
-		if(key==NULL||encryption==NULL||getString==NULL||stringLen<sizeof(char)*strlen(encryption)+30)
-		{
-			sprintf(err,"input wrong");
-			return NULL;
-		}	
-		if(backString!=NULL)
-			free(backString);
-		backString=(char*)malloc(sizeof(char)*strlen(encryption)+30);
-		memset(backString,0,sizeof(char)*strlen(encryption)+30);
-		if(backString==NULL)
-		{
-			sprintf(err,"malloc wrong");
-			return NULL;
-		}
-		for(unsigned int i=0;i<strlen(key);i++)
-			temp+=key[i];
-		int end=time(NULL)+liveSecond+temp;
-		temp=temp%4;
-		for(unsigned int i=0;i<strlen(encryption);i++)
-		{
-			backString[i]=encryption[i]-temp;
-			if(backString[i]==94)
-				backString[i]=92;
-		}
-		char tempString[60]={0},endString[30]={0};
-		sprintf(endString,"%d",end);
-		for(unsigned int i=0;i<strlen(endString);i++)
-			endString[i]+=50;
-		sprintf(tempString,".%s.%c",endString,encryption[0]);
-		strcat(backString,tempString);
-		strcpy(getString,backString);
-		return getString;
-	}
-	const char* decryptToken(const char* key,const char* token,char* buffer,unsigned int bufferLen)
-	{
-		char* temp=strchr((char*)token,'.');
-		if(temp==NULL||key==NULL||token==NULL||buffer==NULL||bufferLen<strlen(token))
-		{
-			sprintf(err,"input wrong");
-			return NULL;
-		}
-		if(backString!=NULL)
-			free(backString);
-		backString=(char*)malloc(sizeof(char)*strlen(token));
-		memset(backString,0,sizeof(char)*strlen(token));
-		char endString[20]={0};
-		if(sscanf(temp+1,"%20[^.]",endString)<=0)
-		{
-			sprintf(err,"get time wrong");
-			return NULL;
-		}
-		int keyTemp=0,end=0;
-		for(unsigned int i=0;i<strlen(endString);i++)
-			endString[i]-=50;
-		sscanf(endString,"%d",&end);
-		for(unsigned int i=0;i<strlen(key);i++)
-			keyTemp+=key[i];
-		end-=keyTemp;
-		if(end-time(NULL)<=0)
-		{
-			sprintf(err,"over time");
-			return NULL;
-		}
-		keyTemp=keyTemp%4;
-		unsigned int i=0;
-		for(i=0;i+token<temp;i++)
-			if(token[i]!=92)
-				backString[i]=token[i]+keyTemp;
-			else
-				backString[i]=97;
-		backString[i+1]=0;
-		if(backString[0]!=token[strlen(token)-1])
-		{
-			sprintf(err,"key wrong");
-			return NULL;
-		}
-		strcpy(buffer,backString);
-		return buffer;
-	}
-	inline const char* LastError()
-	{
-		return err;
-	}
-};
 class Email{
 private:
 	sockaddr_in their_addr;
@@ -2206,7 +2108,7 @@ public:
 	}
 };
 class HttpServer:private ServerTcpIp{
-public://main class for http server2.0.=
+public://main class for http server2.0
 	enum RouteType{//oneway stand for like /hahah,wild if /hahah/*,static is recource static
 		ONEWAY,WILD,STATIC,
 	};
