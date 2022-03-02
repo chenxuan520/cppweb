7bbca884b7dbabf3915e8b2a689e29464ce4f0a3
chenxuan <1143663140@qq.com>
2022-03-02 21:32:21 +0800

    im[rovelog

--- class/threadPoll.cpp
+++ class/threadPoll.cpp
@@ -4,6 +4,7 @@
 	funtion:this file is a try for thead pool
 *********************************/
 #include<pthread.h>
+#include<string.h>
 #include<stdio.h>
 #include<iostream>
 #include<queue>
@@ -95,6 +96,131 @@ private:
 				task.ptask(task.arg);
 			pool.busyNow--;
 		}
+	}
+};
+class LogSystem{
+private:
+	const char* fileName;
+	char* buffer[4];
+	const char* error;
+	char* now;
+	char* page;
+	size_t nowLen;
+	size_t bufferLen;
+	ThreadPool pool;
+	std::queue<char*> nowFree;
+public:
+	LogSystem(const char* name,size_t bufferLen=1024)\
+										   :fileName(name),now(NULL),pool(1)
+	{
+		page=NULL;
+		if(fileName==NULL)
+			fileName="access.log";
+		if(bufferLen<128)
+			bufferLen=128;
+		this->bufferLen=bufferLen;
+		for(unsigned i=0;i<4;i++)
+			buffer[i]=NULL;
+		for(unsigned i=0;i<4;i++)
+		{
+			buffer[i]=(char*)malloc(sizeof(char)*bufferLen);
+			if(buffer[i]==NULL)
+			{
+				error="malloc wrong";
+				return;
+			}
+			nowFree.push(buffer[i]);
+			memset(buffer[i],0,sizeof(char)*bufferLen);
+		}
+		now=nowFree.front();
+		nowFree.pop();
+		page=now;
+	}
+	~LogSystem()
+	{
+		std::pair<LogSystem*,char*>* argv=new std::pair<LogSystem*,char*>(this,page);
+		worker(argv);
+		for(unsigned i=0;i<4;i++)
+			if(buffer[i]!=NULL)
+				free(buffer[i]);
+	}
+	void accessLog(const char* text)
+	{
+		if(text==NULL)
+			return;
+		if(nowLen+strlen(text)>=bufferLen)
+		{
+			while(nowFree.empty());
+			/* pool.mutexLock(); */
+			now=nowFree.front();
+			nowFree.pop();
+			/* pool.mutexUnlock(); */
+			nowLen=0;
+			std::pair<LogSystem*,char*>* argv=new std::pair<LogSystem*,char*>(this,page);
+			ThreadPool::Task task{worker,argv};
+			pool.addTask(task);
+			page=now;
+		}
+		strcat(now,text);
+		strcat(now,"\n");
+		nowLen+=strlen(text)+1;
+	}
+	static void recordRequest(const void* text,int )
+	{
+		static char method[32]={0},askPath[256]={0},buffer[512]={0};
+		static LogSystem loger("access.log");
+		/* int port=0; */
+		sscanf((char*)text,"%32s%256s",method,askPath);
+		/* time_t now=time(NULL); */
+		/* sprintf(buffer,"%s %s %s %s",ctime(&now),ServerTcpIp::getPeerIp(soc,&port),method,askPath); */
+		loger.accessLog(buffer);
+	}
+	static bool recordFileError(const char* fileName)
+	{
+		FILE* fp=fopen("wrong.log","r+");
+		if(fp==NULL)
+			fp=fopen("wrong.log","w+");
+		if(fp==NULL)
+			return false;
+		fseek(fp,0,SEEK_END);
+		fprintf(fp,"open file %s wrong\n",fileName);
+		fclose(fp);
+		return true;
+	}
+	/* static void httpLog(const void * text,int soc) */
+	/* { */
+	/* 	DealHttp http; */
+	/* 	int port=0; */
+	/* 	FILE* fp=fopen("ask.log","a+"); */
+	/* 	if(fp==NULL) */
+	/* 		fp=fopen("ask.log","w+"); */
+	/* 	if(fp==NULL) */
+	/* 		return ; */
+	/* 	DealHttp::Request req; */
+	/* 	http.analysisRequest(req,text); */
+	/* 	time_t now=time(NULL); */
+	/* 	fprintf(fp,"%s %s %s %s\n",ctime(&now),ServerTcpIp::getPeerIp(soc,&port),req.method.c_str(),req.askPath.c_str()); */
+	/* 	fclose(fp); */
+	/* 	return ; */
+	/* } */
+private:
+	static void* worker(void* argv)
+	{
+		std::pair<LogSystem*,char*>& now=*(std::pair<LogSystem*,char*>*)argv;
+		FILE* fp=fopen(now.first->fileName,"a+");
+		if(fp==NULL)
+		{
+			now.first->error="open file wrong";
+			return NULL;
+		}
+		fprintf(fp,"%s",now.second);
+		fclose(fp);
+		memset(now.second,0,now.first->bufferLen);
+		/* now.first->pool.mutexLock(); */
+		now.first->nowFree.push(now.second);
+		/* now.first->pool.mutexUnlock(); */
+		delete (std::pair<LogSystem*,char*>*)argv;
+		return NULL;
 	}
 };
 int temp=0;
