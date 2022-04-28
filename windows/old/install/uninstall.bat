92fd1ab9885de69bba835bbf170308a59b23d7ce
chenxuan <1607772321@qq.com>
2022-04-28 20:44:09 +0800

    addSrc

--- windows/old/install/uninstall.bat
+++
@@ -1,4 +0,0 @@
-@echo off
-rmdir /S /Q ..\x86_64-w64-mingw32\include\cppweb
-rm ..\x86_64-w64-mingw32\lib\lib\http.a ..\x86_64-w64-mingw32\lib\lib\libserver.a ..\x86_64-w64-mingw32\lib\lib\libthreadpool.a
-pause
