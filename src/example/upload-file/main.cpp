3bd3d0562b1dac66d15e4a41494777dfb7965621
chenxuan <1607772321@qq.com>
2022-12-22 18:39:41 +0800

    fix bug I673RK

--- src/example/upload-file/main.cpp
+++ src/example/upload-file/main.cpp
@@ -5,17 +5,25 @@ using namespace std;
 using namespace cppweb;
 void upload(HttpServer& server,DealHttp& http,int soc)
 {
-    char name[100]={0};
+	char name[100]={0};
 	DealHttp::Request req;
-	http.analysisRequest(req,server.recText());
+	if(false==http.analysisRequest(req,server.recText(http)))
+	{
+		Json json={{"status","analysisHttpAsk wrong"}};
+		http.gram.json(DealHttp::STATUSOK,json());
+		return;
+	}
 	if(req.head.find("Content-Length")==req.head.end())
 	{
 		http.gram.body="message wrong";
 		return;
 	}
+	char path[128]={0};
+	http.getWildUrl(server.recText(http),server.getNowRoute()->route,path,sizeof(char)*128);
+	req.askPath=".";
+	req.askPath+=path;
 	int flen=0;
 	sscanf(req.head["Content-Length"].c_str(),"%d",&flen);
-	printf("conlen:%d\n",flen);
 	void *temp=malloc(sizeof(char)*(flen+1000));
 	if(temp==NULL)
 	{
@@ -23,9 +31,9 @@ void upload(HttpServer& server,DealHttp& http,int soc)
 		return;
 	}
 	memset(temp,0,sizeof(char)*(flen+1000));
-	unsigned len=server.getCompleteMessage(soc);
-	printf("len %d\n",len);
-    flen=http.getRecFile(server.recText(),len,name,100,(char*)temp,sizeof(char)*(flen+1000));
+	flen=http.getRecFile(server.recText(http),server.getRecLen(http),name,100,(char*)temp,sizeof(char)*(flen+1000));
+	req.askPath+=name;
+	cout<<"file:"<<req.askPath<<endl;
     FileGet::writeToFile(name,(char*)temp,flen);
 	http.gram.typeFile=DealHttp::JSON;
 	http.gram.body="{\"status\":\"ok\"}";
